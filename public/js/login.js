62cffe6381ba7ae317ef3aa9a47e2eefef946afa
Hannah Lodge <lodg0021@umn.edu>
2022-05-16 11:05:45 -0500

    adding info

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,19 @@
+const { json } = require("express/lib/response")
+
 document.getElementById("submitBtn").addEventListener("click", (e)=>{
     e.preventDefault()
+    const email = document.querySelector('email').value
+    const password = document.querySelector('password').value
+
+    if (email && password){
+        console.log("working login input")
+        const res = await fetch ('/api/login', {
+            method: 'POST',
+            body: JSON.stringify({email, password})
+        })
+    }
+
+    console.log(res)
+
 })
 
